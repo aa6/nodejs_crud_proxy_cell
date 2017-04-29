6ee612751e1a2ca227c00dcce8e4894eda200d86
aa6 <justsunshineandbluesky@gmail.com>
2017-04-29 14:35:36 +0300

    Another test

--- tests/sort.js
+++ tests/sort.js
@@ -46,4 +46,30 @@ describe("Sort functions",function()
         expect(unsorted).toEqual([ [ '4', 'FOUR' ], [ '2', 'TWO' ], [ '5', 'FIVE' ] ])
         expect(sorted).toEqual([ [ '5', 'FIVE' ], [ '4', 'FOUR' ], [ '2', 'TWO' ] ])
     })
+
+    it("sort(fn)",function()
+    {
+        var cell = new crudproxycell()
+        cell.$4 = "FOUR"
+        cell.$2 = "TWO"
+        cell.push("FIVE")
+        unsorted = []
+        for(key in cell.$)
+        {
+            unsorted.push([key,cell.$[key]])
+        }
+
+        cell.sort(function(a,b)
+        {
+            return a.toString() < b.toString()
+        })
+
+        sorted = []
+        for(key in cell.$)
+        {
+            sorted.push([key,cell.$[key]])
+        }
+        expect(unsorted).toEqual([ [ '4', 'FOUR' ], [ '2', 'TWO' ], [ '5', 'FIVE' ] ])
+        expect(sorted).toEqual([ [ '2', 'TWO' ], [ '4', 'FOUR' ], [ '5', 'FIVE' ] ])
+    })
 })
